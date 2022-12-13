59962a10e16edd2ee8c8c195a1b446687c9e1551
leonardogoandete <leonardogoandete>
2022-12-13 19:24:51 -0300

    Ajustado instancia e playbook

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0ecc74eca1d66d8a6"
-  instance_type = "t2.micro"
+  instance_type = "t1.micro"
   key_name      = "iac-alura"
 #  user_data     = <<-EOF
 #                #!/bin/bash
