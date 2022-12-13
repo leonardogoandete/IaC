e9385ae23f825ca965756d80ea77810c63cf683b
leonardogoandete <leonardogoandete>
2022-12-13 17:11:30 -0300

    Implementado Script para start httpd

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0ecc74eca1d66d8a6"
   instance_type = "t2.micro"
-  key_name = "iac-alura"
+  key_name      = "iac-alura"
+  user_data     = <<-EOF
+                #!/bin/bash
+                cd ~
+                echo "<h1>Realizado com Terraform</h1>" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste AWS v5"
   }
 }
