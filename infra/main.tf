ca8ef68913f6366c0ed46f7a6d3af0cbdf2da600
leonardogoandete <leonardogoandete>
2022-12-16 20:02:55 -0300

    Implementado ternario no Auto Scaling

--- infra/main.tf
+++ infra/main.tf
@@ -39,7 +39,7 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
-  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
+  target_group_arns = var.producao ? [ aws_lb_target_group.alvoLoadBalancer.arn ] : []
 }
 
 resource "aws_default_subnet" "subnet_1" {
@@ -53,6 +53,7 @@ resource "aws_default_subnet" "subnet_2" {
 resource "aws_lb" "loadBalancer" {
   internal = false
   subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id  ]
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_lb_target_group" "alvoLoadBalancer" {
@@ -60,6 +61,7 @@ resource "aws_lb_target_group" "alvoLoadBalancer" {
   port = "8000"
   protocol = "HTTP"
   vpc_id = aws_default_vpc.default.id
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_default_vpc" "default" {
@@ -73,6 +75,7 @@ resource "aws_lb_listener" "entradaLoadBalancer" {
     type = "forward"
     target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
   }
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_autoscaling_policy" "escala-Producao" {
@@ -85,4 +88,5 @@ resource "aws_autoscaling_policy" "escala-Producao" {
     }
     target_value = 50.0
   }
+  count = var.producao ? 1 : 0
 }
