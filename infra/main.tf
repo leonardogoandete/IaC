0a910a2dba017160e03f780182f70dc165d86bca
leonardogoandete <leonardogoandete>
2022-12-16 15:22:19 -0300

    Configurado LoadBalancer

--- infra/main.tf
+++ infra/main.tf
@@ -39,6 +39,7 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
+  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
 }
 
 resource "aws_default_subnet" "subnet_1" {
@@ -52,5 +53,24 @@ resource "aws_default_subnet" "subnet_2" {
 resource "aws_lb" "loadBalancer" {
   internal = false
   subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id  ]
-  
+}
+
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_default_vpc" "default" {
+}
+
+resource "aws_lb_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
+  }
 }
